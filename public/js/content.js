7b86a279c275d49e7ecec3edfeb3d7abe67d1479
AlexisIvanVargasPartida <alivvargaspa@ittepic.edu.mx>
2020-03-06 13:41:23 -0700

    LP-009 Porfolio item hover effect and popup window

---
+++ public/js/content.js
@@ -0,0 +1,78 @@
+var topics = {
+    topic1: {
+        topicLabel: "Dark Attribute",
+        topicText: "DARK (闇やみ Yami 'Darkness') monsters are perhaps the most favored of all monsters.",
+        topicImage: 'public/img/1.png',
+    },
+    topic2: {
+        topicLabel: "Light Attribute",
+        topicText: "LIGHT (光ひかり Hikari) monsters are the historic opposites of DARK Monsters.",
+        topicImage: 'public/img/2.png',
+    },
+    topic3: {
+        topicLabel: "Wind Attribute",
+        topicText: "WIND (風かぜ Kaze) is an Attribute commonly associated with Winged Beast-Type monsters, though Insect, Fairy, Dragon, and Psychic-Type WIND monsters are not uncommon.",
+        topicImage: 'public/img/3.png',
+    },
+    topic4: {
+        topicLabel: "Earth Attribute",
+        topicText: "EARTH (地ち Chi) monsters are powerful cards focused on both brute strength and rock-solid defense. EARTH monsters tie with DARK for being among the most numerous and widespread; unless a deck is specifically themed around a particular Attribute, it will almost always have a few EARTH monsters.",
+        topicImage: 'public/img/4.png',
+
+    },
+    topic5: {
+        topicLabel: "Fire Attribute",
+        topicText: "FIRE (炎ほのお Honō 'Fire' or 'Flame') is an Attribute commonly associated with Pyro, Dragon, Dinosaur and Machine-Type monsters, though it is not uncommon in other Types such as Warrior or even Beast. FIRE is diametrically opposed to WATER.",
+        topicImage: 'public/img/5.png',
+    },
+    topic6: {
+        topicLabel: "Watter Attribute",
+        topicText: "WATER (水みず Mizu) is a very versatile Attribute, and even has a semi-viable Deck based around such monsters, the Water Deck. The Monster Types typically associated with WATER are Aqua, Fish, and Sea Serpent, though Plants and Reptiles are not uncommonly WATER monsters. WATER is usually thought of as the opposite to FIRE, though there is no real advantage for either Attribute over each other in the OCG/TCG.",
+        topicImage: 'public/img/6.png',
+    },
+    topic7: {
+        topicLabel: "Divine Attribute",
+        topicText: "DIVINE (神かみ Kami,'God') is one of the seven legal Attributes in the Yu-Gi-Oh! TCG and OCG.",
+        topicImage: 'public/img/7.png',
+    },
+    topic8: {
+        topicLabel: "Spell Card",
+        topicText: "Spell Cards (Japanese: 魔ま法ほうカード Mahō Kādo, or 魔法マジックカード Majikku Kādo 'Magic Card' in early releases of the OCG Series 3 and in the anime; Japanese symbol text: 魔ま Ma or 魔マジック Majikku), called Magic Card in the TCG until the release of Magician's Force, are cards with green-colored borders that have various effects to alter the play of the game.",
+        topicImage: 'public/img/8.png',
+    },
+    topic9: {
+        topicLabel: "Trap Card",
+        topicText: "Trap Cards (Japanese: 罠トラップカード Torappu Kādo) are cards with purple-colored borders that have various effects. A Trap Card must first be Set and can only be activated after the current turn has finished. After that, it may be activated during either player's turn. Trap Cards are Spell Speed 2, with the exception of Counter Trap Cards, which are Spell Speed 3.",
+        topicImage: 'public/img/9.png',
+    },
+    topic10: {
+        topicLabel: "Continous Symbol",
+        topicText: "A Continuous Spell Card, known as a Permanent Magic Card (Japanese: 永えい続ぞく魔ま法ほうカード Eizoku Mahō Kādo) in the OCG, is a Spell Speed 1 Spell Card that stays on the field once it is activated. Spell Ruler, previously known as Magic Ruler, was the first TCG Booster Pack that contained Continuous Spell Cards.",
+        topicImage: 'public/img/10.png',
+    },
+    topic11: {
+        topicLabel: "Field Symbol",
+        topicText: "Field Spell Cards, formerly Field Magic Cards (Japanese: フィールド魔ま法ほうカード Fīrudo Mahō Kādo), have the advantage of being able to change the entire state of play for its controller's or for both players' side of the field. They are Spell Speed 1 and are placed on the Field Zone, apart from the Spell & Trap Zones.",
+        topicImage: 'public/img/11.png',
+    },
+    topic12: {
+        topicLabel: "Equip Symbol",
+        topicText: "An Equip Spell Card, known as an Equip Magic Card (Japanese: 装そう備び魔ま法ほうカード Sōbi Mahō Kādo) in the OCG, is a Spell Card that is a card that is equipped to a face-up monster on the field. Equip Spell Cards are a type of Equip Card; Equip Cards can also be Trap Cards or monsters that are treated as Equip Cards.",
+        topicImage: 'public/img/12.png',
+    },
+    topic13: {
+        topicLabel: "Counter Symbol",
+        topicText: "Counter Trap Cards (Japanese: カウンター罠トラップカード Kauntā Torappu Kādo) are a unique Trap card type that are of Spell Speed 3. Being the only cards/effects that are Spell Speed 3, only other Counter Trap Cards can be activated in response to them. Most of them can only be activated to negate or punish the activations of other cards, or Summons of monsters.",
+        topicImage: 'public/img/13.png',
+    },
+    topic14: {
+        topicLabel: "Ritual Symbol",
+        topicText: "Ritual Spell Cards (Japanese: 儀ぎ式しき魔ま法ほうカード Gishiki Mahō Kādo), known as Ritual Magic Cards in the TCG prior to Magician's Force and the OCG, are a type of Spell Card used to Ritual Summon Ritual Monsters.",
+        topicImage: 'public/img/14.png',
+    },
+    topic15: {
+        topicLabel: "Quick-Play Symbol",
+        topicText: "Quick-Play Spell Cards, known as Instant Magic Cards in the OCG (Japanese: 速そっ攻こう魔ま法ほうカード Sokkō Mahō Kādo), are a type of Spell Card that are Spell Speed 2. The turn player can activate Quick-Play Spell Cards from their hand during any Phase of their turn; either player can activate Set Quick-Play Spell cards during any Phase in either player's turn, except during the turn they are Set.",
+        topicImage: 'public/img/15.png',
+    }
+}
